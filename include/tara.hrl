cf88338ee9f2c81d6f5ce05928a3731b170b9077
brigadier <eugeneim@gmail.com>
2016-08-01 13:57:06 +0300

    iterator defines moved to tara.hrl

--- include/tara.hrl
+++ include/tara.hrl
@@ -9,3 +9,18 @@
 -define(OP_INS(FieldNo, Arg),		[<<"!">>, FieldNo, Arg]).
 -define(OP_ASSIGN(FieldNo, Arg), 	[<<"=">>, FieldNo, Arg]).
 -define(OP_SPLICE(FieldNo, Pos, Offs, Arg), 	[<<":">>, FieldNo, Pos, Offs, Arg]).
+
+
+
+-define(ITERATOR_EQ, 0).
+-define(ITERATOR_REQ, 1).
+-define(ITERATOR_ALL, 2).
+-define(ITERATOR_LT, 3).
+-define(ITERATOR_LE, 4).
+-define(ITERATOR_GE, 5).
+-define(ITERATOR_GT, 6).
+-define(ITERATOR_BITSET_ALL_SET, 7).
+-define(ITERATOR_BITSET_ANY_SET, 8).
+-define(ITERATOR_BITSET_ALL_NOT_SET, 9).
+-define(ITERATOR_OVERLAPS, 10).
+-define(ITERATOR_NEIGHBOR, 11).
